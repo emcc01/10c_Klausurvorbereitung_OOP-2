9c315480bb94e8c82dc7e9a5e77f4a3a4f02cb5c
Eva <e.moritz.comcave@gmail.com>
2023-06-01 10:33:45 +0200

    für klausur

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,12 +4,12 @@ public class App {
     
     public static void main(String[] args) {
 
-
+// Objekt wird gestartet: Testklasse
      Test test = new Test("Grizabella", 29);
 
      System.out.println(test.getName());
 
-
+// Grizabella wird ausgegeben, weil erreichbar durch getter und setter
 
 
 
