8c131a59cd7cf7c457a4fdbb8d3a9e4003f2ccb4
Eva <e.moritz.comcave@gmail.com>
2023-06-01 10:34:06 +0200

    für klausur

--- com/cc/java/Test.java
+++ com/cc/java/Test.java
@@ -3,10 +3,12 @@ package com.cc.java;
 public class Test {
     
     // Felder (fields) | Attribute, Eigenschaften
-    // private --> Kapselung / encapsulation/ data hiding
-
+    // private --> Kapselung / encapsulation: kapselung (der Daten in den Objekten)/ data hiding, damit nicht in Klasse beliebig eingegriffen werden kann, 
+// wie legt man felder an? datentypen angeben? Constructor anlegen, getter setter
     private String name;
     private int age;
+
+
 
     public Test(String name, int age) {
         this.name = name;
@@ -16,6 +18,8 @@ public class Test {
 	// public --> alle dürfen sehen/schreiben (+)
 	// private --> NUR die eigene Klasse / Objekt (-)
 	// protected --> NUR die eigene Klasse / Objekt + Subklassen (#)
+
+    //Kommunikation der privaten Daten mit der Umgebung durch Getter und Setter, ist Programmierschnittstelle
 
     public String getName() {
         return name;
